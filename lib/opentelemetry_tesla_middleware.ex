68b6216c0099f67047e36c1ab1e69aa1b691d3ed
Indrek Juhkam <indrek@urgas.eu>
2021-09-09 11:29:24 +0300

    Fix middleware return value
    [Tesla.Middleware][1] as a function signature:
    ```
    call(env :: Tesla.Env.t(), next :: Tesla.Env.stack(), options :: any()) ::
      Tesla.Env.result()
    ```
    
    where `Tesla.Env.result()` is `{:ok, t()} | {:error, any()}`.
    
    Currently just `env` was returned. This broke other middlewares in the
    chain. Instead, `{:ok, env}` should be returned. We can even just return
    whatever `Tesla.run/2` returns which simplifies to code as well.
    
    [1]: https://hexdocs.pm/tesla/Tesla.Middleware.html

--- lib/opentelemetry_tesla_middleware.ex
+++ lib/opentelemetry_tesla_middleware.ex
@@ -5,12 +5,5 @@ defmodule Tesla.Middleware.OpentelemetryTeslaMiddleware do
     env
     |> Tesla.put_headers(:otel_propagator.text_map_inject([]))
     |> Tesla.run(next)
-    |> case do
-      {:ok, env} ->
-        env
-
-      {:error, error} ->
-        {:error, error}
-    end
   end
 end
